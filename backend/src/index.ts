0b0abde97cbac5684b9d3a0e1dece0beff1db0ee
Purvika <purvikagore@gmail.com>
2024-06-05 22:33:12 +0530

    started

---
+++ backend/src/index.ts
@@ -0,0 +1,38 @@
+import express from "express"
+
+const app = express();
+
+// GET - to get data
+// PUT - Update data
+// POST - send data
+// DELETE - del 
+
+app.get("/hello", (req, res, next) => {
+  return res.send("Hello Purvika");
+});
+
+// app.post("/hello", (req, res, next) => {
+//   return res.send("Hello Purvika");
+// });
+// sirf sedn se nai hota so use app.use
+
+app.use(express.json());
+
+app.post("/hello", (req, res, next) => {
+  console.log(req.body.name);
+  return res.send("Hello Purvika");
+});
+
+app.put("/hello", (req, res, next) => {
+  console.log(req.body.name);
+  return res.send("Hello Purvika");
+});
+
+// /hello is only static. If you want dynamic then:
+// Created the user id which helps to dynamically get it 
+app.delete("/user/:id", (req, res, next) => {
+  console.log(req.params.id);
+  return res.send("Hello Purvika");
+});
+
+app.listen(5000, () => console.log("Server Open"));
