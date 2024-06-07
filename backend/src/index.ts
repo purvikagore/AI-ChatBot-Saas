fe64ec3a3a0cdbf9c37ef68d947748efb444b926
Purvika <purvikagore@gmail.com>
2024-06-07 06:38:40 +0530

    src stuff

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,38 +1,55 @@
 import express from "express"
-
+import { config } from "dotenv";
+config(); //used to connect to mongo
 const app = express();
 
-// GET - to get data
-// PUT - Update data
-// POST - send data
-// DELETE - del 
-
-app.get("/hello", (req, res, next) => {
-  return res.send("Hello Purvika");
-});
-
-// app.post("/hello", (req, res, next) => {
-//   return res.send("Hello Purvika");
-// });
-// sirf sedn se nai hota so use app.use
-
+// middleware
 app.use(express.json());
 
-app.post("/hello", (req, res, next) => {
-  console.log(req.body.name);
-  return res.send("Hello Purvika");
-});
+// connection and listeners
+app.listen(5000, () => console.log("Server is open"));
 
-app.put("/hello", (req, res, next) => {
-  console.log(req.body.name);
-  return res.send("Hello Purvika");
-});
 
-// /hello is only static. If you want dynamic then:
-// Created the user id which helps to dynamically get it 
-app.delete("/user/:id", (req, res, next) => {
-  console.log(req.params.id);
-  return res.send("Hello Purvika");
-});
 
-app.listen(5000, () => console.log("Server Open"));
+
+
+
+
+// import express from "express"
+
+// const app = express();
+
+// // GET - to get data
+// // PUT - Update data
+// // POST - send data
+// // DELETE - del 
+
+// app.get("/hello", (req, res, next) => {
+//   return res.send("Hello Purvika");
+// });
+
+// // app.post("/hello", (req, res, next) => {
+// //   return res.send("Hello Purvika");
+// // });
+// // sirf sedn se nai hota so use app.use
+
+// app.use(express.json());
+
+// app.post("/hello", (req, res, next) => {
+//   console.log(req.body.name);
+//   return res.send("Hello Purvika");
+// });
+
+// app.put("/hello", (req, res, next) => {
+//   console.log(req.body.name);
+//   return res.send("Hello Purvika");
+// });
+
+// // /hello is only static. If you want dynamic then:
+// // Created the user id which helps to dynamically get it 
+// app.delete("/user/:id", (req, res, next) => {
+//   console.log(req.params.id);
+//   return res.send("Hello Purvika");
+// });
+
+// app.listen(5000, () => console.log("Server Open"));
