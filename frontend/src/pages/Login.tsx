a4a33248aafaa5a6fabf1c900da8eac44ad2f787
Purvika <purvikagore@gmail.com>
2024-06-27 16:21:09 +0530

    frontend

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Box width={"100%"} height={"100%"} display="flex" flex={1}>
+      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
+        <img src="airobot.png" alt="Robot" style={{ width: "400px" }} />
+      </Box>
+      <Box display={"flex"} flex={{ xs: 1, md: 0.5 }} justifyContent={'center'} alignItems={'center'} padding={2} ml={'auto'} mt = {16}></Box>
+    <form action="" style = {{margin:'auto',padding:'30px',boxShadow:"10px 10px 20px #0000"}}></form>
+    
+    </Box>
+  );
+};
 
-export default Login
+export default Login;
